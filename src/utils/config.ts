c01f98178e158950b211c326444f6a877dc9f748
Ashutosh <ashu.singh212@gmail.com>
2024-05-20 09:41:53 +0530

    fix: env vars

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,10 +2,10 @@ export const getConfig = () => ({
   secret: process.env.SECRET,
   redisServer: process.env.REDIS_SERVER,
   redisPassword: process.env.REDIS_PASSWORD,
-  redisPort: process.env.REDIS_PASSWORD || 6379,
+  redisPort: process.env.REDIS_PORT || 6379,
   apiUrl: process.env.API_URL,
   googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleServiceAccount: process.env.GCP_SERVICE_ACCOUNT_PVT_FILE,
   googleGroupEmail: process.env.GOOGLE_GROUP_EMAIL
 });
